f470fb8a452167f6bfb84ec61a51a431dc512693
Danendra <ndaamustofa@gmail.com>
2022-05-31 13:53:21 +0700

    menambahkan data yang di update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import snmp from 'snmp-node';
 import db from './config/database.js'
 import cors from "cors"
-import irdRoutes from './routes/irdRoute.js'
+// import irdRoutes from './routes/irdRoute.js'
 
 const app = express();
 const session = new snmp.Session();
@@ -12,11 +12,9 @@ try {
     setInterval(() => {
         for (let id = 1; id <= 6; id++) {
             let ip = id + 1
-            session.getAll({ oids: ['.1.3.6.1.4.1.1773.1.3.208.2.2.6.0', '.1.3.6.1.4.1.1773.1.3.208.4.1.2.0', '.1.3.6.1.4.1.1773.1.3.208.2.2.2.0', '.1.3.6.1.4.1.1773.1.3.208.3.3.9.0', '.1.3.6.1.4.1.1773.1.3.208.3.3.3.0', '.1.3.6.1.4.1.1773.1.3.208.3.4.4.1.3.1', '.1.3.6.1.4.1.1773.1.3.208.3.3.8.0', '.1.3.6.1.4.1.1773.1.3.208.3.3.7.0'], host: `192.168.112.${ip}`, community: 'public' }, function (error, data) {
+            session.getAll({ oids: ['.1.3.6.1.4.1.1773.1.3.208.2.2.6.0', '.1.3.6.1.4.1.1773.1.3.208.4.1.2.0', '.1.3.6.1.4.1.1773.1.3.208.2.2.2.0', '.1.3.6.1.4.1.1773.1.3.208.3.3.9.0', '.1.3.6.1.4.1.1773.1.3.208.3.3.3.0', '.1.3.6.1.4.1.1773.1.3.208.3.4.4.1.3.1', '.1.3.6.1.4.1.1773.1.3.208.3.3.8.0', '.1.3.6.1.4.1.1773.1.3.208.3.3.7.0', '.1.3.6.1.4.1.1773.1.3.208.2.1.8.0', '.1.3.6.1.4.1.1773.1.3.208.3.4.2.1.2.1', '.1.3.6.1.4.1.1773.1.3.208.3.4.2.1.2.2'], host: `192.168.112.${ip}`, community: 'public' }, function (error, data) {
                 console.log("##############################")
-                if (error) {
-                    console.log('Fail :(');
-                } else {
+                try {
                     const margin = data[0].value.replace(' +', '').replace('dB', '');
                     const service = data[1].value;
                     const status_sat = data[2].value;
@@ -49,7 +47,15 @@ try {
                         status_ip = "UNLOCKED"
                     }
                     console.log(`kualitas : ${kualitas}`);
-                    db.query(`UPDATE snmps SET video_bitrate = '${bitrate}', kualitas = '${kualitas}', status_sat = '${status_sat}', margin = '${margin}', status_ip = '${status_ip}', service = '${service}', status_video = '${status_video}' WHERE ID = ${id}`);
+                    let ts_bitrate = data[8].value;
+                    console.log(`ts_bitrate : ${ts_bitrate}`)
+                    let pid_audio1 = data[9].value;
+                    console.log(`PID_audio 1 : ${pid_audio1}`)
+                    let pid_audio2 = data[10].value;
+                    console.log(`PID_audio 2 : ${pid_audio2}`)
+                    db.query(`UPDATE snmps SET video_bitrate = '${bitrate}', kualitas = '${kualitas}', status_sat = '${status_sat}', margin = '${margin}', status_ip = '${status_ip}', service = '${service}', status_video = '${status_video}', ts_bitrate = '${ts_bitrate}',PID_audio='${pid_audio1}',PID_audio2='${pid_audio2}' WHERE ID = ${id}`);
+                } catch (error) {
+                    console.log('Fail :(', error);
                 }
             });
         }
